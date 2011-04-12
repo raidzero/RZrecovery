45de3fb658cc79541d025c126c9a4fce2a75caf0
raidzero <paul.raidzero@gmail.com>
2011-04-11 20:28:36 -0400

    massive ui changes

--- ui.c
+++ ui.c
@@ -24,13 +24,18 @@
 #include <sys/time.h>
 #include <time.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
 
 #include "common.h"
 #include "minui/minui.h"
 #include "recovery_ui.h"
 
 #define MAX_COLS 64
-#define MAX_ROWS 32
+#define MAX_ROWS 40
+
+#define MENU_MAX_COLS 64
+#define MENU_MAX_ROWS 250
 
 #define CHAR_WIDTH 10
 #define CHAR_HEIGHT 18
@@ -74,10 +79,11 @@ static time_t gProgressScopeTime, gProgressScopeDuration;
 static int gPagesIdentical = 0;
 
 // Log text overlay, displayed when a magic key is pressed
-static char text[MAX_ROWS][MAX_COLS];
+static char text[MENU_MAX_ROWS][MENU_MAX_COLS];
 static int text_cols = 0, text_rows = 0;
 static int text_col = 0, text_row = 0, text_top = 0;
 static int show_text = 0;
+static int menu_show_start = 0; //menu start position
 
 static char menu[MAX_ROWS][MAX_COLS];
 static int show_menu = 0;
@@ -152,6 +158,26 @@ static void draw_text_line(int row, const char* t) {
 // Should only be called with gUpdateMutex locked.
 static void draw_screen_locked(void)
 {
+	//define menu color integers
+	int cRv;
+	int cGv;
+	int cBv;
+	//define menu highlight text color
+	int txt;
+	
+	ensure_root_path_mounted("CACHE:");
+	
+	if( access("/cache/rgb", F_OK ) != -1 ) {
+		FILE *fp = fopen ("/cache/rgb", "rb");
+		fread(&cRv, 1, 1, fp);
+		fread(&cGv, 1, 1, fp);
+		fread(&cBv, 1, 1, fp);
+		fread(&txt, 1, 1, fp);
+		fclose(fp);	
+	} else {
+		set_color(54,74,255);
+	}
+	
     draw_background_locked(gCurrentIcon);
     draw_progress_locked();
 
@@ -160,30 +186,46 @@ static void draw_screen_locked(void)
         gr_fill(0, 0, gr_fb_width(), gr_fb_height());
 
         int i = 0;
+		int j = 0;
+		int row = 0;
         if (show_menu) {
-            gr_color(64, 96, 255, 255);
-            gr_fill(0, (menu_top+menu_sel) * CHAR_HEIGHT,
-                    gr_fb_width(), (menu_top+menu_sel+1)*CHAR_HEIGHT+1);
+ 	    gr_color(cRv,cGv,cBv,255);
+		gr_fill(0, (menu_top + menu_sel - menu_show_start) * CHAR_HEIGHT,
+			gr_fb_width(), (menu_top + menu_sel - menu_show_start + 1)*CHAR_HEIGHT+1);
 
-            for (; i < menu_top + menu_items; ++i) {
+		
+			for (i = 0; i < menu_top; ++i) {
+			draw_text_line(i, menu[i]);
+			row++;
+		}	
+			
+		if (menu_items - menu_show_start + menu_top >= MAX_ROWS)	
+			j = MAX_ROWS - menu_top;
+		else
+			j = menu_items - menu_show_start;
+			
+			
+            for (i = menu_show_start + menu_top; i < (menu_show_start + menu_top + j); ++i) {
                 if (i == menu_top + menu_sel) {
-                    gr_color(255, 255, 255, 255);
-                    draw_text_line(i, menu[i]);
-                    gr_color(64, 96, 255, 255);
-                } else {
-                    draw_text_line(i, menu[i]);
+                    gr_color(txt,txt,txt,255);
+                    draw_text_line(i - menu_show_start , menu[i]);
+					gr_color(cRv,cGv,cBv,255);
+			} else {
+					gr_color(cRv,cGv,cBv,255);
+                    draw_text_line(i - menu_show_start, menu[i]);
                 }
+				row++;
             }
-            gr_fill(0, i*CHAR_HEIGHT+CHAR_HEIGHT/2-1,
-                    gr_fb_width(), i*CHAR_HEIGHT+CHAR_HEIGHT/2+1);
-            ++i;
+            gr_fill(0, row*CHAR_HEIGHT+CHAR_HEIGHT/2-1,
+                    gr_fb_width(), row*CHAR_HEIGHT+CHAR_HEIGHT/2+1);
+			row++;
         }
+		
+        TEXTCOLOR // text on the bottom
 
-        gr_color(255, 255, 0, 255);
-
-        for (; i < text_rows; ++i) {
-            draw_text_line(i, text[(i+text_top) % text_rows]);
-        }
+	    for (; row < text_rows; ++row) {
+		draw_text_line(row, text[(row+text_top) % text_rows]);
+	    }
     }
 }
 
@@ -217,7 +259,7 @@ static void *progress_thread(void *cookie)
 
         // update the progress bar animation, if active
         // skip this if we have a text overlay (too expensive to update)
-        if (gProgressBarType == PROGRESSBAR_TYPE_INDETERMINATE && !show_text) {
+        if (gProgressBarType == PROGRESSBAR_TYPE_INDETERMINATE ) {
             update_progress_locked();
         }
 
@@ -335,6 +377,11 @@ void ui_init(void)
     pthread_t t;
     pthread_create(&t, NULL, progress_thread, NULL);
     pthread_create(&t, NULL, input_thread, NULL);
+    
+    pthread_mutex_lock(&gUpdateMutex);
+    show_text = !show_text;
+    update_screen_locked();
+    pthread_mutex_unlock(&gUpdateMutex);
 }
 
 void ui_set_background(int icon)
@@ -425,7 +472,7 @@ void ui_print(const char *fmt, ...)
     pthread_mutex_unlock(&gUpdateMutex);
 }
 
-void ui_start_menu(char** headers, char** items) {
+void ui_start_menu(char** headers, char** items, int sel) {
     int i;
     pthread_mutex_lock(&gUpdateMutex);
     if (text_rows > 0 && text_cols > 0) {
@@ -435,14 +482,15 @@ void ui_start_menu(char** headers, char** items) {
             menu[i][text_cols-1] = '\0';
         }
         menu_top = i;
-        for (; i < text_rows; ++i) {
+        for (; i < MENU_MAX_ROWS; ++i) {
             if (items[i-menu_top] == NULL) break;
             strncpy(menu[i], items[i-menu_top], text_cols-1);
             menu[i][text_cols-1] = '\0';
         }
         menu_items = i - menu_top;
         show_menu = 1;
-        menu_sel = 0;
+	menu_show_start = 0;
+        menu_sel = sel;
         update_screen_locked();
     }
     pthread_mutex_unlock(&gUpdateMutex);
@@ -454,9 +502,27 @@ int ui_menu_select(int sel) {
     if (show_menu > 0) {
         old_sel = menu_sel;
         menu_sel = sel;
-        if (menu_sel < 0) menu_sel = 0;
-        if (menu_sel >= menu_items) menu_sel = menu_items-1;
+
+        if (menu_sel < 0) {
+			menu_sel = menu_items - 1;
+			menu_show_start = menu_items -(text_rows - menu_top);
+			if(menu_show_start < 0) { menu_show_start = 0; }
+			} else if (menu_sel >= menu_items) {
+			menu_sel = 0;
+			menu_show_start = 0;
+		} 
+
+
+        if (menu_sel < menu_show_start && menu_show_start > 0) {
+            menu_show_start--;
+        }
+
+        if (menu_sel - menu_show_start + menu_top >= text_rows) {
+            menu_show_start++;
+        }
+
         sel = menu_sel;
+		
         if (menu_sel != old_sel) update_screen_locked();
     }
     pthread_mutex_unlock(&gUpdateMutex);
