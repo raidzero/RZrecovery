455b5be51ae0d6ca9603c25ed307392d0a3fe95d
raidzero <paul.raidzero@gmail.com>
2011-11-07 14:46:55 -0700

    take the graphics.c from clockwork to support atrix and other wierd framebuffers, as well as invertred screens

--- minui/graphics.c
+++ minui/graphics.c
@@ -29,15 +29,19 @@
 
 #include <pixelflinger/pixelflinger.h>
 
-#include "font_10x18.h"
+#ifndef BOARD_LDPI_RECOVERY
+	#include "font_10x18.h"
+#else
+	#include "font_7x16.h"
+#endif
+
 #include "minui.h"
 
-typedef struct
-{
-  GGLSurface texture;
-  unsigned cwidth;
-  unsigned cheight;
-  unsigned ascent;
+typedef struct {
+    GGLSurface texture;
+    unsigned cwidth;
+    unsigned cheight;
+    unsigned ascent;
 } GRFont;
 
 static GRFont *gr_font = 0;
@@ -52,305 +56,284 @@ static int gr_vt_fd = -1;
 
 static struct fb_var_screeninfo vi;
 
-static int
-get_framebuffer (GGLSurface * fb)
+static int get_framebuffer(GGLSurface *fb)
 {
-  int fd;
-  struct fb_fix_screeninfo fi;
-  void *bits;
+    int fd;
+    struct fb_fix_screeninfo fi;
+    void *bits;
 
-  fd = open ("/dev/graphics/fb0", O_RDWR);
-  if (fd < 0)
-	  {
-	    perror ("cannot open fb0");
-	    return -1;
-	  }
+    fd = open("/dev/graphics/fb0", O_RDWR);
+    if (fd < 0) {
+        perror("cannot open fb0");
+        return -1;
+    }
 
-  if (ioctl (fd, FBIOGET_FSCREENINFO, &fi) < 0)
-	  {
-	    perror ("failed to get fb0 info");
-	    close (fd);
-	    return -1;
-	  }
+    if (ioctl(fd, FBIOGET_FSCREENINFO, &fi) < 0) {
+        perror("failed to get fb0 info");
+        close(fd);
+        return -1;
+    }
 
-  if (ioctl (fd, FBIOGET_VSCREENINFO, &vi) < 0)
-	  {
-	    perror ("failed to get fb0 info");
-	    close (fd);
-	    return -1;
-	  }
+    if (ioctl(fd, FBIOGET_VSCREENINFO, &vi) < 0) {
+        perror("failed to get fb0 info");
+        close(fd);
+        return -1;
+    }
 
-  bits = mmap (0, fi.smem_len, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-  if (bits == MAP_FAILED)
-	  {
-	    perror ("failed to mmap framebuffer");
-	    close (fd);
-	    return -1;
-	  }
+    bits = mmap(0, fi.smem_len, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if (bits == MAP_FAILED) {
+        perror("failed to mmap framebuffer");
+        close(fd);
+        return -1;
+    }
 
-  fb->version = sizeof (*fb);
-  fb->width = vi.xres;
-  fb->height = vi.yres;
-  fb->stride = vi.xres;
-  fb->data = bits;
-  fb->format = GGL_PIXEL_FORMAT_RGB_565;
-  memset (fb->data, 0, vi.yres * vi.xres * 2);
+    fb->version = sizeof(*fb);
+    fb->width = vi.xres;
+    fb->height = vi.yres;
+#ifdef BOARD_HAS_JANKY_BACKBUFFER
+    fb->stride = fi.line_length/2;
+#else
+    fb->stride = vi.xres;
+#endif
+    fb->data = bits;
+    fb->format = GGL_PIXEL_FORMAT_RGB_565;
+    memset(fb->data, 0, vi.yres * vi.xres * 2);
 
-  fb++;
+    fb++;
 
-  fb->version = sizeof (*fb);
-  fb->width = vi.xres;
-  fb->height = vi.yres;
-  fb->stride = vi.xres;
-  fb->data = (void *) (((unsigned) bits) + vi.yres * vi.xres * 2);
-  fb->format = GGL_PIXEL_FORMAT_RGB_565;
-  memset (fb->data, 0, vi.yres * vi.xres * 2);
+    fb->version = sizeof(*fb);
+    fb->width = vi.xres;
+    fb->height = vi.yres;
+#ifdef BOARD_HAS_JANKY_BACKBUFFER
+    fb->stride = fi.line_length/2;
+    fb->data = (void*) (((unsigned) bits) + vi.yres * fi.line_length);
+#else
+    fb->stride = vi.xres;
+    fb->data = (void*) (((unsigned) bits) + vi.yres * vi.xres * 2);
+#endif
+    fb->format = GGL_PIXEL_FORMAT_RGB_565;
+    memset(fb->data, 0, vi.yres * vi.xres * 2);
 
-  return fd;
+    return fd;
 }
 
-static void
-get_memory_surface (GGLSurface * ms)
-{
-  ms->version = sizeof (*ms);
+static void get_memory_surface(GGLSurface* ms) {
+  ms->version = sizeof(*ms);
   ms->width = vi.xres;
   ms->height = vi.yres;
   ms->stride = vi.xres;
-  ms->data = malloc (vi.xres * vi.yres * 2);
+  ms->data = malloc(vi.xres * vi.yres * 2);
   ms->format = GGL_PIXEL_FORMAT_RGB_565;
 }
 
-static void
-set_active_framebuffer (unsigned n)
+static void set_active_framebuffer(unsigned n)
 {
-  if (n > 1)
-    return;
-  vi.yres_virtual = vi.yres * 2;
-  vi.yoffset = n * vi.yres;
-  vi.bits_per_pixel = 16;
-  if (ioctl (gr_fb_fd, FBIOPUT_VSCREENINFO, &vi) < 0)
-	  {
-	    perror ("active fb swap failed");
-	  }
+    if (n > 1) return;
+    vi.yres_virtual = vi.yres * 2;
+    vi.yoffset = n * vi.yres;
+    vi.bits_per_pixel = 16;
+    if (ioctl(gr_fb_fd, FBIOPUT_VSCREENINFO, &vi) < 0) {
+        perror("active fb swap failed");
+    }
 }
 
-void
-gr_flip (void)
+void gr_flip(void)
 {
-  GGLContext *gl = gr_context;
+    GGLContext *gl = gr_context;
 
-  /* swap front and back buffers */
-  gr_active_fb = (gr_active_fb + 1) & 1;
+    /* swap front and back buffers */
+    gr_active_fb = (gr_active_fb + 1) & 1;
 
-  /* copy data from the in-memory surface to the buffer we're about
-   * to make active. */
-  memcpy (gr_framebuffer[gr_active_fb].data, gr_mem_surface.data,
-	  vi.xres * vi.yres * 2);
+#ifdef BOARD_HAS_FLIPPED_SCREEN
+    /* flip buffer 180 degrees for devices with physicaly inverted screens */
+    unsigned int i;
+    for (i = 1; i < (vi.xres * vi.yres); i++) {
+        unsigned short tmp = gr_mem_surface.data[i];
+        gr_mem_surface.data[i] = gr_mem_surface.data[(vi.xres * vi.yres * 2) - i];
+        gr_mem_surface.data[(vi.xres * vi.yres * 2) - i] = tmp;
+    }
+#endif
 
-  /* inform the display driver */
-  set_active_framebuffer (gr_active_fb);
+    /* copy data from the in-memory surface to the buffer we're about
+     * to make active. */
+    memcpy(gr_framebuffer[gr_active_fb].data, gr_mem_surface.data,
+           vi.xres * vi.yres * 2);
+
+    /* inform the display driver */
+    set_active_framebuffer(gr_active_fb);
 }
 
-void
-gr_color (unsigned char r, unsigned char g, unsigned char b, unsigned char a)
+void gr_color(unsigned char r, unsigned char g, unsigned char b, unsigned char a)
 {
-  GGLContext *gl = gr_context;
-  GGLint color[4];
-
-  color[0] = ((r << 8) | r) + 1;
-  color[1] = ((g << 8) | g) + 1;
-  color[2] = ((b << 8) | b) + 1;
-  color[3] = ((a << 8) | a) + 1;
-  gl->color4xv (gl, color);
+    GGLContext *gl = gr_context;
+    GGLint color[4];
+    color[0] = ((r << 8) | r) + 1;
+    color[1] = ((g << 8) | g) + 1;
+    color[2] = ((b << 8) | b) + 1;
+    color[3] = ((a << 8) | a) + 1;
+    gl->color4xv(gl, color);
 }
 
-int
-gr_measure (const char *s)
+int gr_measure(const char *s)
 {
-  return gr_font->cwidth * strlen (s);
+    return gr_font->cwidth * strlen(s);
 }
 
-int
-gr_text (int x, int y, const char *s)
+int gr_text(int x, int y, const char *s)
 {
-  GGLContext *gl = gr_context;
-  GRFont *font = gr_font;
-  unsigned off;
+    GGLContext *gl = gr_context;
+    GRFont *font = gr_font;
+    unsigned off;
 
-  y -= font->ascent;
+    y -= font->ascent;
 
-  gl->bindTexture (gl, &font->texture);
-  gl->texEnvi (gl, GGL_TEXTURE_ENV, GGL_TEXTURE_ENV_MODE, GGL_REPLACE);
-  gl->texGeni (gl, GGL_S, GGL_TEXTURE_GEN_MODE, GGL_ONE_TO_ONE);
-  gl->texGeni (gl, GGL_T, GGL_TEXTURE_GEN_MODE, GGL_ONE_TO_ONE);
-  gl->enable (gl, GGL_TEXTURE_2D);
+    gl->bindTexture(gl, &font->texture);
+    gl->texEnvi(gl, GGL_TEXTURE_ENV, GGL_TEXTURE_ENV_MODE, GGL_REPLACE);
+    gl->texGeni(gl, GGL_S, GGL_TEXTURE_GEN_MODE, GGL_ONE_TO_ONE);
+    gl->texGeni(gl, GGL_T, GGL_TEXTURE_GEN_MODE, GGL_ONE_TO_ONE);
+    gl->enable(gl, GGL_TEXTURE_2D);
 
-  while ((off = *s++))
-	  {
-	    off -= 32;
-	    if (off < 96)
-		    {
-		      gl->texCoord2i (gl, (off * font->cwidth) - x, 0 - y);
-		      gl->recti (gl, x, y, x + font->cwidth,
-				 y + font->cheight);
-		    }
-	    x += font->cwidth;
-	  }
+    while((off = *s++)) {
+        off -= 32;
+        if (off < 96) {
+            gl->texCoord2i(gl, (off * font->cwidth) - x, 0 - y);
+            gl->recti(gl, x, y, x + font->cwidth, y + font->cheight);
+        }
+        x += font->cwidth;
+    }
 
-  return x;
+    return x;
 }
 
-void
-gr_fill (int x, int y, int w, int h)
+void gr_fill(int x, int y, int w, int h)
 {
-  GGLContext *gl = gr_context;
-
-  gl->disable (gl, GGL_TEXTURE_2D);
-  gl->recti (gl, x, y, w, h);
+    GGLContext *gl = gr_context;
+    gl->disable(gl, GGL_TEXTURE_2D);
+    gl->recti(gl, x, y, w, h);
 }
 
-void
-gr_blit (gr_surface source, int sx, int sy, int w, int h, int dx, int dy)
-{
-  if (gr_context == NULL)
-	  {
-	    return;
-	  }
-  GGLContext *gl = gr_context;
+void gr_blit(gr_surface source, int sx, int sy, int w, int h, int dx, int dy) {
+    if (gr_context == NULL) {
+        return;
+    }
+    GGLContext *gl = gr_context;
 
-  gl->bindTexture (gl, (GGLSurface *) source);
-  gl->texEnvi (gl, GGL_TEXTURE_ENV, GGL_TEXTURE_ENV_MODE, GGL_REPLACE);
-  gl->texGeni (gl, GGL_S, GGL_TEXTURE_GEN_MODE, GGL_ONE_TO_ONE);
-  gl->texGeni (gl, GGL_T, GGL_TEXTURE_GEN_MODE, GGL_ONE_TO_ONE);
-  gl->enable (gl, GGL_TEXTURE_2D);
-  gl->texCoord2i (gl, sx - dx, sy - dy);
-  gl->recti (gl, dx, dy, dx + w, dy + h);
+    gl->bindTexture(gl, (GGLSurface*) source);
+    gl->texEnvi(gl, GGL_TEXTURE_ENV, GGL_TEXTURE_ENV_MODE, GGL_REPLACE);
+    gl->texGeni(gl, GGL_S, GGL_TEXTURE_GEN_MODE, GGL_ONE_TO_ONE);
+    gl->texGeni(gl, GGL_T, GGL_TEXTURE_GEN_MODE, GGL_ONE_TO_ONE);
+    gl->enable(gl, GGL_TEXTURE_2D);
+    gl->texCoord2i(gl, sx - dx, sy - dy);
+    gl->recti(gl, dx, dy, dx + w, dy + h);
 }
 
-unsigned int
-gr_get_width (gr_surface surface)
-{
-  if (surface == NULL)
-	  {
-	    return 0;
-	  }
-  return ((GGLSurface *) surface)->width;
+unsigned int gr_get_width(gr_surface surface) {
+    if (surface == NULL) {
+        return 0;
+    }
+    return ((GGLSurface*) surface)->width;
 }
 
-unsigned int
-gr_get_height (gr_surface surface)
-{
-  if (surface == NULL)
-	  {
-	    return 0;
-	  }
-  return ((GGLSurface *) surface)->height;
+unsigned int gr_get_height(gr_surface surface) {
+    if (surface == NULL) {
+        return 0;
+    }
+    return ((GGLSurface*) surface)->height;
 }
 
-static void
-gr_init_font (void)
+static void gr_init_font(void)
 {
-  GGLSurface *ftex;
-  unsigned char *bits, *rle;
-  unsigned char *in, data;
+    GGLSurface *ftex;
+    unsigned char *bits, *rle;
+    unsigned char *in, data;
 
-  gr_font = calloc (sizeof (*gr_font), 1);
-  ftex = &gr_font->texture;
+    gr_font = calloc(sizeof(*gr_font), 1);
+    ftex = &gr_font->texture;
 
-  bits = malloc (font.width * font.height);
+    bits = malloc(font.width * font.height);
 
-  ftex->version = sizeof (*ftex);
-  ftex->width = font.width;
-  ftex->height = font.height;
-  ftex->stride = font.width;
-  ftex->data = (void *) bits;
-  ftex->format = GGL_PIXEL_FORMAT_A_8;
+    ftex->version = sizeof(*ftex);
+    ftex->width = font.width;
+    ftex->height = font.height;
+    ftex->stride = font.width;
+    ftex->data = (void*) bits;
+    ftex->format = GGL_PIXEL_FORMAT_A_8;
 
-  in = font.rundata;
-  while ((data = *in++))
-	  {
-	    memset (bits, (data & 0x80) ? 255 : 0, data & 0x7f);
-	    bits += (data & 0x7f);
-	  }
+    in = font.rundata;
+    while((data = *in++)) {
+        memset(bits, (data & 0x80) ? 255 : 0, data & 0x7f);
+        bits += (data & 0x7f);
+    }
 
-  gr_font->cwidth = font.cwidth;
-  gr_font->cheight = font.cheight;
-  gr_font->ascent = font.cheight - 2;
+    gr_font->cwidth = font.cwidth;
+    gr_font->cheight = font.cheight;
+    gr_font->ascent = font.cheight - 2;
 }
 
-int
-gr_init (void)
+int gr_init(void)
 {
-  gglInit (&gr_context);
-  GGLContext *gl = gr_context;
+    gglInit(&gr_context);
+    GGLContext *gl = gr_context;
 
-  gr_init_font ();
-  gr_vt_fd = open ("/dev/tty0", O_RDWR | O_SYNC);
-  if (gr_vt_fd < 0)
-	  {
-	    // This is non-fatal; post-Cupcake kernels don't have tty0.
-	    // so why even mention it?
-	    // perror ("can't open /dev/tty0");
-	  }
-  else if (ioctl (gr_vt_fd, KDSETMODE, (void *) KD_GRAPHICS))
-	  {
-	    // However, if we do open tty0, we expect the ioctl to work.
-	    perror ("failed KDSETMODE to KD_GRAPHICS on tty0");
-	    gr_exit ();
-	    return -1;
-	  }
+    gr_init_font();
+    gr_vt_fd = open("/dev/tty0", O_RDWR | O_SYNC);
+    if (gr_vt_fd < 0) {
+        // This is non-fatal; post-Cupcake kernels don't have tty0.
+        perror("can't open /dev/tty0");
+    } else if (ioctl(gr_vt_fd, KDSETMODE, (void*) KD_GRAPHICS)) {
+        // However, if we do open tty0, we expect the ioctl to work.
+        perror("failed KDSETMODE to KD_GRAPHICS on tty0");
+        gr_exit();
+        return -1;
+    }
 
-  gr_fb_fd = get_framebuffer (gr_framebuffer);
-  if (gr_fb_fd < 0)
-	  {
-	    gr_exit ();
-	    return -1;
-	  }
+    gr_fb_fd = get_framebuffer(gr_framebuffer);
+    if (gr_fb_fd < 0) {
+        gr_exit();
+        return -1;
+    }
 
-  get_memory_surface (&gr_mem_surface);
+    get_memory_surface(&gr_mem_surface);
 
-  fprintf (stderr, "framebuffer: fd %d (%d x %d)\n",
-	   gr_fb_fd, gr_framebuffer[0].width, gr_framebuffer[0].height);
+    fprintf(stderr, "framebuffer: fd %d (%d x %d)\n",
+            gr_fb_fd, gr_framebuffer[0].width, gr_framebuffer[0].height);
 
-  /* start with 0 as front (displayed) and 1 as back (drawing) */
-  gr_active_fb = 0;
-  set_active_framebuffer (0);
-  gl->colorBuffer (gl, &gr_mem_surface);
+        /* start with 0 as front (displayed) and 1 as back (drawing) */
+    gr_active_fb = 0;
+    set_active_framebuffer(0);
+    gl->colorBuffer(gl, &gr_mem_surface);
 
-  gl->activeTexture (gl, 0);
-  gl->enable (gl, GGL_BLEND);
-  gl->blendFunc (gl, GGL_SRC_ALPHA, GGL_ONE_MINUS_SRC_ALPHA);
+    gl->activeTexture(gl, 0);
+    gl->enable(gl, GGL_BLEND);
+    gl->blendFunc(gl, GGL_SRC_ALPHA, GGL_ONE_MINUS_SRC_ALPHA);
 
-  return 0;
+    return 0;
 }
 
-void
-gr_exit (void)
+void gr_exit(void)
 {
-  close (gr_fb_fd);
-  gr_fb_fd = -1;
+    close(gr_fb_fd);
+    gr_fb_fd = -1;
 
-  free (gr_mem_surface.data);
+    free(gr_mem_surface.data);
 
-  ioctl (gr_vt_fd, KDSETMODE, (void *) KD_TEXT);
-  close (gr_vt_fd);
-  gr_vt_fd = -1;
+    ioctl(gr_vt_fd, KDSETMODE, (void*) KD_TEXT);
+    close(gr_vt_fd);
+    gr_vt_fd = -1;
 }
 
-int
-gr_fb_width (void)
+int gr_fb_width(void)
 {
-  return gr_framebuffer[0].width;
+    return gr_framebuffer[0].width;
 }
 
-int
-gr_fb_height (void)
+int gr_fb_height(void)
 {
-  return gr_framebuffer[0].height;
+    return gr_framebuffer[0].height;
 }
 
-gr_pixel *
-gr_fb_data (void)
+gr_pixel *gr_fb_data(void)
 {
-  return (unsigned short *) gr_mem_surface.data;
+    return (unsigned short *) gr_mem_surface.data;
 }
+
